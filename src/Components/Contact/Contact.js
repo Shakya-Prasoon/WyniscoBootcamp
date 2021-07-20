a56e0fa900c3cb0dc7985db508005f2ceb3bb30b
Prasoon Shakya <pras95shakya@gmail.com>
2021-07-20 01:53:46 -0700

    homework #2.2, fixed link

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -11,9 +11,8 @@ function Contact() {
         <Container fluid>
           <h1 className="display-3">My Contact Details</h1>
           <p className="lead">Here are my contact details </p>
-          <p className="lead">Email : sachin@wynisco.com my email </p>
-          <p className="lead">Phone : 732 874 0188 </p>
-          <p className="lead">Typical Wake up / Sleep Hours : Other than bootcamp. I will available morning 7AM to 11AM EST via slack or whatsapp Changed  </p>
+          <p className="lead">Email : shakya95pras@gmail.com my email </p>
+          <p className="lead">I'm in West Coast so the time zone is PST.</p>
         </Container>
       </Jumbotron>
     </div>
