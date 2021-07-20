d1c5c01b22c08e71f55849d6a334eef8b75e4366
Prasoon Shakya <pras95shakya@gmail.com>
2021-07-20 16:46:42 -0700

    changed basic contact info

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -12,7 +12,8 @@ function Contact() {
           <h1 className="display-3">My Contact Details</h1>
           <p className="lead">Here are my contact details </p>
           <p className="lead">Email : shakya95pras@gmail.com my email </p>
-          <p className="lead">I'm in West Coast so the time zone is PST.</p>
+          <p className="lead">I'm in West Coast, so the time zone is PST.</p>
+          <p className="lead">Feel free to reach me out via Whats app or Slack.</p>
         </Container>
       </Jumbotron>
     </div>
